68c527f8df58762773f50956963b73663a1971a2
Vitalii Bilostotskyi <forcedarv@gmail.com>
2026-04-27 11:26:45 +0300

    fix: remove margin-top from footer component for consistent styling

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {
 
 const Footer = ({ copyright }: FooterProps) => {
   return (
-    <footer className='flex items-center justify-center w-full bg-[#09090b] text-white p-4 mt-4'>
+    <footer className='flex items-center justify-center w-full bg-[#09090b] text-white p-4'>
       <p className='text-zinc-300 text-sm'>{copyright}</p>
     </footer>
   );
